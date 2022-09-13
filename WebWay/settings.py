4042f1399a57a2da951a02fef7e32127a9ade6fb
Ali Eyvazi <88749639+Ali-Eyvazi@users.noreply.github.com>
2022-09-13 11:24:24 +0430

    Update settings.py

--- WebWay/settings.py
+++ WebWay/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_eikn(*6=0xi%ryez@(jftmy*4&3w@4$80gd($k*^lu4_m8x6+'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
